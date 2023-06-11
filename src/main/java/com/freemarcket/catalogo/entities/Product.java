220ede82494cadbe6f6cbfc787a5054bb9126d51
Thallis Henrique Ferreira <83103510+THenriqueh@users.noreply.github.com>
2023-06-11 17:27:17 -0300

    Update Product.java

---
+++ src/main/java/com/freemarcket/catalogo/entities/Product.java
@@ -0,0 +1,39 @@
+package com.freemarcket.catalogo.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter private Long id;
+    @Getter @Setter private String name;
+    @Column(columnDefinition = "TEXT")
+    @Getter @Setter private String description;
+    @Getter @Setter private Double price;
+    
+    @ManyToMany(mappedBy = "storages")
+    @Getter Set<Storage> storages = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    @Getter Set<Category> categories = new HashSet<>();
+
+}
