0fd4033e317e5638f606b7022192b66e42058b7e
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2023-06-11 17:22:48 -0300

    Update Category.java

---
+++ src/main/java/com/freemarcket/catalogo/entities/Category.java
@@ -0,0 +1,28 @@
+package com.freemarcket.catalogo.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter private Long id;
+    @Getter @Setter private String name;
+    @ManyToMany (mappedBy = "categories")
+    @Getter
+    Set <Product> products = new HashSet<>();
+
+}
