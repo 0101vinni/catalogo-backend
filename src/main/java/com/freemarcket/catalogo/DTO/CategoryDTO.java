58bbaa75fc9be450c6f2ea8a085c48c7b98996fd
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2023-06-11 17:22:30 -0300

    Update CategoryDTO.java

---
+++ src/main/java/com/freemarcket/catalogo/DTO/CategoryDTO.java
@@ -0,0 +1,26 @@
+package com.freemarcket.catalogo.DTO;
+
+import java.io.Serializable;
+
+import com.freemarcket.catalogo.entities.Category;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class CategoryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Getter @Setter private Long id;
+    @Getter @Setter private String name;
+
+    public CategoryDTO (Category entity){
+        this.id=entity.getId();
+        this.name=entity.getName();
+    }
+
+
+
+}
